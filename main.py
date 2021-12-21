55b9136d943712d1cb8b56f6fc4fff6609f30b32
Elijah R <tomahawkmissile2@gmail.com>
2021-12-21 05:19:59 -0800

    Fixed byte array writing to write to FPGA.

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ def main():
     file.close()
     print("Successfully read file: "+str(binary))
 
-    ser = serial.Serial(port, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=2) # 9600 baud, 2 second timeout
-    ser.open()
+    ser = serial.Serial(port, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=5) # 9600 baud, 2 second timeout
 
     print("Restarting Mojo board...")
     ser.setDTR(False)
@@ -49,22 +48,36 @@ def main():
     ser.flushOutput()
     print("Done.")
 
-    ser.write(b'F') # F for flash
-    if(ser.read(1)[0] != b'R'):
+    time.sleep(500/1000)
+
+    ser.write(b'R') # F for flash
+    resp = ser.read(1)[0]
+    print("Got response: "+str(resp))
+    if(resp.to_bytes(1,'big') != b'R'):
         print("Mojo did not respond! Make sure the port is set correctly!")
         sys.exit(-1)
 
     ser.write(file_len.to_bytes(4,'big'))
 
-    if(ser.read(1)[0] != 'O'):
+    resp = ser.read(1)[0]
+    print("Got response: "+str(resp))
+    if(resp.to_bytes(1,'big') != b'O'):
         print("Mojo did not acknowledge transfer size!")
         sys.exit(-1)
 
-    print("Uploading...")
+    print("Binary size: "+str(file_len)+" bytes")
+    print("Uploading (this may take a minute)...")
     byte_arr = bytearray(binary_data)
     ser.write(byte_arr)
+    print("Done uploading")
 
-    if(ser.read(1)[0] != 'D'):
+    try:
+        resp = ser.read(1)[0]
+    except IndexError:
+        print("No response has been received!")
+        sys.exit(-1)
+    print("Got response: "+str(resp))
+    if(resp.to_bytes(1,'big') != b'D'):
         print("Mojo did not acknowledge the transfer! This means that your code has likely not been uploaded! Try running this program again.")
         sys.exit(-1)
 
